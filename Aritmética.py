bd47a2935acc236164721103dc24604f8cdeab28
Thayná Souza de Oliveira <thaina_olliveira@hotmail.com>
2024-07-12 11:48:14 -0300

    Criei novo arquivo: MyOperation e aritmética

---
+++ Aritmética.py
@@ -0,0 +1,17 @@
+# Demonstração do uso de operadores
+X = int(input("Digite um número:"))
+Y = int(input("Digite outro número:"))
+
+SOMA = X+Y
+print("O resultado da soma é:",SOMA)
+
+SUBTRACAO = X-Y
+print("O resultado da subtração é", SUBTRACAO)
+
+MULTIPLICACAO = X*Y
+print("O resultado da multiplicação é:", MULTIPLICACAO)
+
+DIVISAO = X/Y
+print("O resultado da divisão é", DIVISAO)
+
+
---
+++ MyOperation.py
@@ -0,0 +1,15 @@
+# Demonstração de expressões complexas
+
+# Declaração de variáveis que iremos usar...
+X = 10; Y = 20; Z = 30
+
+valor = X * (Y / 10 + Z / 10) - 25
+print("O resultado da expressão #1 é:", valor)
+
+valor = ((Y - X) + (Z - X)) / 3
+print("O resultado da expressão #2 é:", valor)
+
+valor = (X + (Y * 2 + (Z / 10))) - 18
+print("O resultado da expressão #3 é:", valor)
+
+#Antes de executar estes códigos,seria possível calcular "de cabeça" estes valores?

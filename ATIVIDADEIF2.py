8a6be7b06956b09adf13744065938a7bec3b6b77
Thayná Souza de Oliveira <thaina_olliveira@hotmail.com>
2024-07-15 11:56:25 -0300

    alterações no exercicio 2

--- ATIVIDADEIF2.py
+++ ATIVIDADEIF2.py
@@ -1,18 +1,16 @@
 #Construa um programa onde o usuário digitará quatro notas trimestrais e o programa irá calcular a média
-X = float(input("Digite o valor da nota 1:"))
-Y = float(input("Digite o valor da nota 2:"))
-Z = float(input("Digite o valor da nota 3:"))
-W = float(input("Digite o valor da nota 4:"))
+x = int(input("Digite o valor da nota 1:"))
+y = int(input("Digite o valor da nota 2:"))
+z = int(input("Digite o valor da nota 3:"))
+w = int(input("Digite o valor da nota 4:"))
 
-soma = X + Y + Z + W
+soma = x + y + z + w
 media = soma / 4
 
 print(f"A soma é {soma}")
 print(f"A média é {media}")
 
 if media > 6:
-print("Aluno reprovado")
-
+    print("Aluno reprovado")
 else:
-print("Aluno aprovado")
-
+    print("Aluno aprovado")

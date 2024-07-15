aa2828d6ea03046011269211ca11078dc93930c5
Thayná Souza de Oliveira <thaina_olliveira@hotmail.com>
2024-07-15 09:10:02 -0300

    Hoje fizemos o exercícios de Algortimos e lógica de programação

---
+++ MEDIA.py
@@ -0,0 +1,11 @@
+# Demonstração do uso de operadores...
+# Criar um programa que faça o somatório de 3 valiores e tire a sua média...
+
+X = int(input("Digite o valor 1:"))
+Y = int(input("Digite o valor 2:"))
+Z = int(input("Digite o valor 3:"))
+SOMA = X + Y + Z
+MEDIA = SOMA / 3
+
+print("A soma é:", SOMA)
+print(f"A média é {MEDIA}")

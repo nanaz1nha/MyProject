d6539181ed5dff95fddee03ee529a541ad24931c
Thayná Souza de Oliveira <thaina_olliveira@hotmail.com>
2024-07-17 10:34:09 -0300

    correção das atividades da aula passada

--- IfParaEscola.py
+++ IfParaEscola.py
@@ -1,16 +1,18 @@
 #Construa um programa onde o usuário digitará quatro notas trimestrais e o programa irá calcular a média
-x = int(input("Digite o valor da nota 1:"))
-y = int(input("Digite o valor da nota 2:"))
-z = int(input("Digite o valor da nota 3:"))
-w = int(input("Digite o valor da nota 4:"))
+n1 = int(input("Digite o valor da nota 1:"))
+n2 = int(input("Digite o valor da nota 2:"))
+n3 = int(input("Digite o valor da nota 3:"))
+n4 = int(input("Digite o valor da nota 4:"))
 
-soma = x + y + z + w
+soma = n1 + n2 + n3 + n4
 media = soma / 4
 
 print(f"A soma é {soma}")
 print(f"A média é {media}")
 
-if media > 6:
-    print("Aluno reprovado")
-else:
+if media >= 6:
     print("Aluno aprovado")
+else:
+    print("Aluno reprovado")
+
+

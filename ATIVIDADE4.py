c19ef117aa2ea19adc21e1f3d956740514564b9b
Thayná Souza de Oliveira <thaina_olliveira@hotmail.com>
2024-07-19 11:50:17 -0300

    novos exercicios sendo corrigidos

--- ATIVIDADE4.py
+++ ATIVIDADE4.py
@@ -2,7 +2,7 @@
 posicao = input("Digite a função de campo do jogador:")
 
 if posicao == "goleiro" or posicao == "zagueiro" or posicao == "lateral":
-    print("Defesa")
+    print("Defesa!")
 elif posicao == "volante" or posicao == "meia":
     print("Meio-Campo!")
 elif posicao == "ponta" or posicao == "atacante" or posicao == "centroavante":

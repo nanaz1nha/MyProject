667364eb6293c7fbb35d6c88f9f4d1faefee8bb3
nanaz1nha <thaina_olliveira@hotmail.com>
2024-08-16 11:28:48 -0300

    Add files via upload

---
+++ MyAccountBank.py
@@ -0,0 +1,27 @@
+# Demonstração de OOP em Python...
+from abc import ABC, abstractmethod
+
+class cliente:
+    @abstractmethod
+    def __init__(self, titular, conta, saldo): 
+        self.titular = titular
+        self.conta = conta
+        self.saldo = saldo
+
+class cliente_fisico(cliente):
+    def apresentar(self):
+        print("Olá! Eu sou:", self.titular)
+        print("Minha conta é:", self.conta)
+        print("E quero saber o meu saldo")
+        return
+
+    
+# Para criar uma instância baesada na classe cliente...
+fulano = cliente_fisico("João", "423.05025-21" , 25000.00)
+
+# Executado o método da instância criada...
+fulano.apresentar()
+
+# Acessando os atributos das instâncias criadas...
+print("De fato, você realmente é:", fulano.titular)
+print("No momento, a sua conta possui: R$ ", fulano.saldo)
